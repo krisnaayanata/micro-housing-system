ed7b992cd54bc23d44bbb79c7780a58e770dc25b
gentasankara <62679608+gentasankara@users.noreply.github.com>
2020-04-02 01:52:38 +0800

    Add files via upload

--- residence/add_residence_process.php
+++ residence/add_residence_process.php
@@ -1,12 +1,15 @@
 <?php
 	include "../koneksi.php";
 
+	$userID=$_POST['userID'];
 	$address=$_POST['address'];
 	$numUnits=$_POST['numUnits'];
 	$sizePerUnit=$_POST['sizePerUnit'];
 	$monthlyRental=$_POST['monthlyRental'];
 
-	$sql="insert into residence values('','$address','$numUnits','$sizePerUnit','$monthlyRental')";
+	$sql="insert into residence values('','$userID','$address','$numUnits','$sizePerUnit','$monthlyRental')";
+
+	
 
 	$query=mysql_query($sql);
   if($query)
